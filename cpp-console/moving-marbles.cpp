4337110632334a5805830eb22a4aa58eb994dc5c
Artur Dębkowski <arturdebkowski@gmail.com>
2016-08-05 15:16:30 +0100

    process test cases

--- cpp-console/moving-marbles.cpp
+++ cpp-console/moving-marbles.cpp
@@ -1,16 +1,40 @@
 #include <iostream>
 #include <fstream>
+#include <cstdio>
+#include <string>
 
 using namespace std;
 
+// void println(std::string s)
+// {
+// 	cout >> s >> endl;
+// }
+
+void process_test_case()
+{
+	int n;
+	cin >> n;
+	cout << "n = " << n << endl;
+
+	for (int i = 0; i < n; i++)
+	{
+
+	}
+}
+
 int main()
 {
-	ifstream in("simple_input.txt");
-	int t;
+	freopen("simple_input.txt", "r", stdin);
+	// ifstream in("simple_input.txt");
+	int t, n;
 
-	in >> t;
+	cin >> t;
+    cout << "t = " << t << endl;
 
-    cout << "t = " << t;
+	for(int tc = 0; tc < t; tc++)
+	{
+		process_test_case();
+	}
 
     return 0;
 }
