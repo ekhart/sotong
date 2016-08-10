f82a1dd0722d85d2696fe150afe18ef9f16b7750
Artur Dębkowski <arturdebkowski@gmail.com>
2016-08-10 14:13:25 +0100

    add simple input test case, housing-areas.cpp example read T & PRINT_EXPR turn on/off with DEBUG symbol

---
+++ cpp-console/housing-areas/housing-areas.cpp
@@ -0,0 +1,49 @@
+// In Practice, You should use the statndard input/output
+// in order to receive a score properly.
+// Do not use file input and output. Please be very careful.
+
+#include <cstdio>
+#include <iostream>
+
+using namespace std;
+
+
+#define DEBUG 0		// comment out to turn off PRINT_EXPR
+
+#ifdef DEBUG
+	#define PRINT_EXPR(a) cout << (#a) << " = " << (a) << endl
+#else
+	#define PRINT_EXPR(a) do { } while(0)
+#endif
+
+
+int main(int argc, char** argv)
+{
+	int tc, T;
+
+	// The freopen function below opens input.txt file in read only mode, and afterward,
+	// the program will read from input.txt file instead of standard(keyboard) input.
+	// To test your program, you may save input data in input.txt file,
+	// and use freopen function to read from the file when using cin function.
+	// You may remove the comment symbols(//) in the below statement and use it.
+	// Use #include<cstdio> or #include<stdio.h> to use the function in your program.
+	// But before submission, you must remove the freopen function or rewrite comment symbols(//).
+
+	freopen("input.txt", "r", stdin);
+
+	cin >> T;
+
+	PRINT_EXPR(T);
+
+	for(tc = 0; tc < T; tc++)
+	{
+		/**********************************
+		*  Implement your algorithm here. *
+		***********************************/
+
+		// Print the answer to standard output(screen).
+
+	}
+
+	return 0;//Your program should return 0 on normal termination.
+}
