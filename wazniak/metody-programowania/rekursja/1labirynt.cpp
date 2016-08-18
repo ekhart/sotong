4a04323038bd6603823192358f5d9dc0d053bbc4
Artur Dębkowski <arturdebkowski@gmail.com>
2016-08-18 13:02:36 +0200

    basic recursion task init

---
+++ wazniak/metody-programowania/rekursja/1labirynt.cpp
@@ -0,0 +1,18 @@
+#include <cstdio>
+#include <iostream>
+
+using namespace std;
+
+// Zadanie 1 (Labirynt)
+// Czy istnieje ścieżka miedzy wskazanymi punktami (i1,j1) i (i2,j2) 
+// w labiryncie reprezentowanym przez prostokątną tablicę liczb całkowitych o rozmiarze M×N, 
+// zawierającą zera (ściana) i jedynki (droga)? 
+// Zakładamy, że nie można przechodzić z pola na pole po skosie (np. z (2,5) na (3,6)), 
+// a tylko w czterech podstawowych kierunkach (np. z (2,5) na (3,5), (2,4) itd.) 
+
+int main(int argc, char** argv)
+{
+    cout << "hello" << endl;
+
+    return 0;
+}
